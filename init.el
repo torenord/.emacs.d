7e118604b1ae90ea5fb0ba9ea6a1a41550e78ef4
Tore Sund Norderud <tore@kikora.no>
2018-01-31 14:21:05 +0100

    .

--- init.el
+++ init.el
@@ -13,8 +13,8 @@
 ;; Disable modes
 (blink-cursor-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (tooltip-mode -1)
 
 ;; Enable modes
